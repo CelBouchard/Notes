176f0eee7afc74810b4e72305b337720439a38f2
CelBouchard <31857121+CelBouchard@users.noreply.github.com>
2017-09-29 11:41:04 -0400

    Update notes.java

--- notes.java
+++ notes.java
@@ -1,2 +1,12 @@
-// comment
-public static int 
+// PROJECT 002, Matrixes
+
+public Vector(Vector v) { }
+  // Creates a new vector, and we want it to be a copy of the local vector we just made. 
+
+public void scalarAddInPlace(double d) { }
+  // Adds value d to all elements in vector.
+
+public Vector scalarAdd(double d) { 
+  // TODO, insert the following:
+  // Vector.set = new Vector(this), set.scalarAddInPlace(d), return set;
+}
